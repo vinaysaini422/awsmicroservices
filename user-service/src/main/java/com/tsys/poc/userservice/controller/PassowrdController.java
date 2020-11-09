a7fb536e6b0144165c67affd259d4cacf1a41b9f
vsaini <vsaini@tsys.com>
2020-11-09 12:12:27 +0530

    aws poc

--- user-service/src/main/java/com/tsys/poc/userservice/controller/PassowrdController.java
+++ user-service/src/main/java/com/tsys/poc/userservice/controller/PassowrdController.java
@@ -1,5 +1,7 @@
 package com.tsys.poc.userservice.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,19 +11,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tsys.poc.userservice.config.JWTValidator;
 import com.tsys.poc.userservice.model.Constants;
 import com.tsys.poc.userservice.model.PasswordRequestModel;
 import com.tsys.poc.userservice.model.PasswordUpdateModel;
 import com.tsys.poc.userservice.model.ResponseModel;
 import com.tsys.poc.userservice.service.UserService;
 
+import lombok.AllArgsConstructor;
+
 
 @RestController
 @RequestMapping("/passwordReset")
+@AllArgsConstructor
 public class PassowrdController {
 
-	@Autowired
 	UserService userService;
+	
+	private JWTValidator jWTValidator;
+	
 
 	@PostMapping("/request")
 	ResponseEntity<ResponseModel> resetPassword(@RequestBody PasswordRequestModel passwordRequestModel) {
@@ -33,9 +41,11 @@ public class PassowrdController {
 	}
 
 	@PutMapping("/update")
-	ResponseEntity<ResponseModel> updatePassword(@RequestBody PasswordUpdateModel passwordUpdateModel) {
+	ResponseEntity<ResponseModel> updatePassword(@RequestBody PasswordUpdateModel passwordUpdateModel, HttpServletRequest request) {
 		ResponseModel rM = new ResponseModel();
-
+		if (!jWTValidator.validateToken(request)) {
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
 		userService.updatePassword(passwordUpdateModel, rM);
 		rM.setOperationName(Constants.PASSWORD_UPDATE.name());
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).body(rM);
